dc9fff111c3c6d81544122226ef24018ccca4a98
MYEONGGI <smgsmg226@gmail.com>
2022-11-02 09:46:43 +0900

    2022-11-02

---
+++ codingapple/Level 2/탭기능 만들기.js
@@ -0,0 +1,16 @@
+// 버튼 0누르면
+// 모든 버튼에 붙은 orange 클래스 제거
+// 버튼 0에 붙은 orange 클래스 추가
+// 모든 div에 붙은 show 클래스 제거
+// div에 show 클래스명 추가
+
+
+for(let i=0; i<3; i++){
+    $('.tab-button').eq(i).click(function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(i).addClass('orange') 
+    // $(this).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(i).addClass('show');
+});    
+}
