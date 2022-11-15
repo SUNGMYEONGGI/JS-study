8d13385ef5639946adb51fbb9ff2eb0bc1bf06e5
MYEONGGI <smgsmg226@gmail.com>
2022-11-15 23:03:22 +0900

    이벤트 버블링

--- codingapple/Level 2/탭기능 만들기.js
+++ codingapple/Level 2/탭기능 만들기.js
@@ -14,3 +14,15 @@ for(let i=0; i<3; i++){
     $('.tab-content').eq(i).addClass('show');
 });    
 }
+
+
+$('.list').click(function(e){
+    탭열기(e.target.dataset.id)
+})
+
+function 탭열기(숫자){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(숫자).addClass('orange') 
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(숫자).addClass('show');
+}
